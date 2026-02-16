16244eb862e15aa4bf737f44849b2f4921e4638e
Evan ngugi wambui <evanngugi547@gmail.com>
2026-02-16 20:02:22 +0300

    fix out of boumds error

--- 01-Arrays/Insertion/c/begin.c
+++ 01-Arrays/Insertion/c/begin.c
@@ -2,24 +2,26 @@
 
 int main()
 {
-    int arr[6] = {1, 2, 3, 4, 5};
+    int n = 5;
+    // leave an extra space for new element to be added
+    int arr[] = {1, 2, 3, 4, 5, 0};
     int length = sizeof(arr) / sizeof(int);
     int i;
     int element = 10; // element to insert
 
     // reverse traverse
-    for (i = length - 1; i >= 0; i--)
+    for (i = n; i >= 0; i--)
     {
         // shift each element to the right
-        arr[ i + 1] = arr[i];
-        // insert at start
-        if(i == 0) {
-            arr[i] = element;
-        }
+        arr[i] = arr[i - 1];
+     
 
     }
 
-    // print the array
+    // insert at start
+    arr[0] = element;
+
+    // print the new array
     for (i = 0; i < length; i++)
     {
         printf("%d, ", arr[i]);
