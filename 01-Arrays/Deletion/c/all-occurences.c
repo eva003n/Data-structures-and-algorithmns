d6d88b07b36a4800b9542a083bb950593bed2560
Evan ngugi wambui <evanngugi547@gmail.com>
2026-02-23 20:13:25 +0300

    add deletion of all occurrences

--- 01-Arrays/Deletion/c/all-occurences.c
+++ 01-Arrays/Deletion/c/all-occurences.c
@@ -4,47 +4,24 @@
 int main()
 {
     int arr[] = {10, 20, 20, 20, 30};
-    int i, j, n = sizeof(arr) / sizeof(arr[0]);
+
+    int i, j = 0, n = sizeof(arr) / sizeof(arr[0]);
     int target = 20;
-
-    // Solution 1 time complexity -> O(n^2) space -> O(1)
-       for (i = 0; i < n; i++)
-       {
-
-           if (arr[i] == target)
-           {
-               for (j = i + 1; j < n; j++)
-               {
-                   // shift element in right to the left
-                   arr[i] = arr[j];
-
-                   // update i to current value of j
-                   i = j - 1;
-               }
-
-               // dynamically reduce array size
-               n--;
-           }
-       } 
-
-    // Solution 2: time complexity -> O(n) space -> O(1)
-    // bool found = false;
-
-  /*   for (i = 0; i < n; i++)
+  
+    // Solution 1 time complexity -> O(n) space -> O(1)
+    for (i = 0; i < n; i++)
     {
-        if (found)
+        if (arr[i] != target)
         {
-            arr[i - 1] = arr[i];
-        }
-        else if (arr[i] == target)
-        {
-            found = true;
+            arr[j] = arr[i];
+
+            j++;
         }
     }
+    n = j;
 
-    if (found)
-        n--; */
-
+    // Solution 2: time complexity -> O(n) space -> O(1)
+    
     // print new array
     for (i = 0; i < n; i++)
     {
