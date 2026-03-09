f069567f41e74b896a602c638c45568a0773ea5f
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-09 10:38:16 +0300

    add circular linked list

--- 02-Linked-list/circular-linked-list/circular.js
+++ 02-Linked-list/circular-linked-list/circular.js
@@ -5,14 +5,20 @@ class Node {
   }
 
   static printLinkedList(head) {
-    let node = head;
+    let curr = head;
     let output = "";
-    while (node !== null) {
-      output += node.data;
-      if (node.next) output += "->";
 
-      node = node.next;
-    }
+    // empty list
+    if(head === null) return null
+    do {
+      // enable printing last node before the condition is checked
+      output += curr.data;
+      // add arrow as long as the next node is not the head
+      if (curr.next !== head) output += "->";
+      // move to the next node
+      curr = curr.next;
+    } while(curr !== head);
+
     return output;
   }
 }
@@ -21,6 +27,6 @@ const head = new Node(10);
 head.next = new Node(40);
 head.next.next = new Node(30);
 head.next.next.next = new Node(20);
-head.next.next.next.next = head
+head.next.next.next.next = head;
 
-console.log(Node.printLinkedList(head))
+console.log(Node.printLinkedList(head));
