c1767703fb301f8fad06fe4d4ef68639e9f898af
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-12 12:19:22 +0300

    add simple queue and circular queue

---
+++ 04-Queue/circular.js
@@ -0,0 +1,72 @@
+class Queue {
+  constructor(capacity) {
+    // storage
+    this.arr = new Array(capacity);
+    // index of front element
+    this.front = 0;
+    // current size of queue
+    this.size = 0;
+    // maximum size of queue
+    this.capacity = capacity;
+  }
+
+  // insert element at rear
+  enqueue(element) {
+    // prevent overflow
+    if (this.size === this.capacity - 1) {
+      throw new Error("Queue overflow");
+    }
+
+    // insert element at rear = (front + size) % capacity ensures circular behavior
+    let rear = (this.front + this.size++) % this.capacity;
+    this.arr[rear] = element;
+  }
+
+  // remove and return front element
+  dequeue() {
+    //prevent undeflow
+    if (this.size === 0) {
+      throw new Error("Queue underflow");
+    }
+    let front = this.arr[this.front];
+    // move front forward by circular movement
+    this.front = (this.front + 1) % this.capacity;
+    // update the size
+    this.size--;
+
+    return this.arr[front];
+  }
+
+  getFront() {
+    //when queue is empty
+    if (this.size === 0) {
+      console.log("Queue is empty");
+      return -1;
+    }
+
+    return this.arr[this.front];
+  }
+
+  getRear() {
+    //when queue is empty
+    if (this.size === 0) {
+      console.log("Queue is empty");
+      return -1;
+    }
+
+    let rear = (this.front + this.size - 1) % this.capacity
+    return this.arr[rear]
+  }
+}
+
+let q1 = new Queue()
+q1.enqueue(10)
+q1.enqueue(20)
+q1.enqueue(30)
+q1.enqueue(40)
+
+console.log(`Front: ${q1.getFront()}`, `Rear: ${q1.getRear()}`)
+q1.dequeue()
+console.log(`Front: ${q1.getFront()}`, `Rear: ${q1.getRear()}`);
+q1.dequeue()
+console.log(`Front: ${q1.getFront()}`, `Rear: ${q1.getRear()}`);
--- 04-Queue/simple-array.js
+++ 04-Queue/simple-array.js
@@ -1,87 +1,98 @@
+// implementing queue with a fixed size array
+/**
+ * The problem with this implementation is that the dequeue takes time O(n) which is not acceptable, both enqueue and dequeue should take Time of O(1) 
+ * That why a better approach to implement queue is not to use simple queue but rather  circular queue
+ * In simple queue we also waste space
+ */
 class Queue {
-    constructor(cap) {
-        // storage
-        this.arr = new Array(cap)
-        // size of queue
-        this.size = 0
-        // rear pointer to determine the next available space to insert element
-        this.rear = 0 
-        // front pointer to keep tack of the next element added to due
-        this.front = 0
-        // maximum size
-        this.capacity = cap
+  constructor(cap) {
+    // storage
+    this.arr = new Array(cap);
+    // track current size of queue
+    this.size = 0;
+
+    // front pointer to keep tack of the next element added to due
+    this.front = 0;
+    // maximum size of the array
+    this.capacity = cap;
+  }
+
+  enqueue(data) {
+    //prevent queue overflow
+    if (this.size === this.cap - 1) {
+      throw new Error("Queue overflow");
+    }
+    // insert element at rear/back of queue(Time O(1) space O(1))
+    this.arr[this.size++] = data;
+  }
+
+  dequeue() {
+    //prevent underflow
+    if (this.size === 0) {
+      throw new Error("Queue underflow");
     }
 
-    enqueue(data) {
-        //prevent queue overflow
-        if(this.rear === this.cap - 1) {
-            throw new Error("Queue overflow")
-        }
-        // insert element at rear/back of queue
-        this.arr[this.rear++] = data
-        
-        // update size
-        this.size++
+    // remove element from front(Time O(n) space(1))
+    this.arr = shift(this.arr, --this.size);
+    return this.arr;
+  }
+
+  getFront() {
+    //prevent underflow
+    if (this.rear === 0) {
+      console.error("Queue underflow, empty queue");
+      return -1
     }
-
-    dequeue() {
-        //prevent underflow
-        if(this.rear === 0) {
-            throw new Error("Queue underflow");
-        }
-
-        // remove element from front 
-        this.arr = shift(this.arr)
-
-        // update front pointer
-        // this.front++
-
-        this.size--
-        return this.arr
+    //element at front (Time O(1) space O(1))
+    return this.arr[this.front];
+  }
+  getRear() {
+    //prevent underflow
+    if (this.rear === 0) {
+      console.error("Queue underflow, empty queue");
+      return -1
     }
+    //element at front (Time O(1) space O(1))
+    return this.arr[this.front + this.size - 1];
+  }
 
-    peek() {
-                //prevent underflow
-        if(this.rear === 0) {
-            throw new Error("Queue underflow");
-        }
-        return this.arr[this.front]
-    }
+  _size() {
+    return this.size;
+  }
 
-    _size() {
-        return this.size
-    }
-
-    isFull() {
-        return this.rear === this.cap - 1
-    }
-    isEmpty() {
-        return this.rear === 0
-    }
+  isFull() {
+    return this.size === this.cap - 1;
+  }
+  isEmpty() {
+    return this.size === 0;
+  }
 }
 
-function shift(arr) {
-    //length of array
-    let n = arr.length
-    let i
-    for( i = 1; i < n; i++) {
-        arr[i - 1] = arr[i]
-    }
-    // elements deleted are replaced by 0 since this is a fixed array
-    arr[--n] = 0;
-    return arr
+function shift(arr, index) {
+  //length of array
+  let n = arr.length;
+//   shiting all the elements that come after first element to the left
+  for (let i = 1; i < n; i++) {
+    arr[i - 1] = arr[i];
+  }
+  // elements deleted are replaced by 0 since this is a fixed array
+  arr[index] = 0;
+  return arr;
 }
 
-let q1 = new Queue(4)
-q1.enqueue(10)
-q1.enqueue(20)
-q1.enqueue(30)
-q1.enqueue(40)
+let q1 = new Queue(4);
+q1.enqueue(10);
+q1.enqueue(20);
+q1.enqueue(30);
+q1.enqueue(40);
 
-console.log("Dequeued: " + q1.dequeue())
-console.log("Peek: " + q1.peek())
-console.log("Size: " + q1._size())
-console.log("Is full: " + q1.isFull())
-console.log("Is empty: " + q1.isEmpty())
 console.log("Dequeued: " + q1.dequeue());
-console.log("Peek: " + q1.peek());
+console.log("Front: " + q1.getFront());
+console.log("Rear: " + q1.getRear());
+console.log("Size: " + q1._size());
+console.log("Is queue empty: " + (q1.isEmpty() ? "Yes" : "No"));
+console.log("Is queue full: " + (q1.isFull() ? "Yes" : "No"));
+console.log("Dequeued: " + q1.dequeue());
+console.log("Front: " + q1.getFront());
+console.log("Rear: " + q1.getRear());
+console.log("Size: " + q1._size());
