961a62234164add6fc3209fdbe268c8de87b82d7
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-20 11:37:03 +0300

    fix bug causing infinite loop

--- 05-Hash-Table/Open-addressing/linearprobing.js
+++ 05-Hash-Table/Open-addressing/linearprobing.js
@@ -1,3 +1,5 @@
+const DELETED = Symbol("Deleted");
+
 class HashNode {
   constructor(key, value) {
     this.key = key;
@@ -8,23 +10,25 @@ class HashNode {
 class HashTable {
   #table;
   #bucketsCount;
-  constructor(buckets) {
+  constructor(buckets = 4) {
     this.#bucketsCount = buckets;
-    this.#table = new Array(buckets).fill(null);
+    this.#table = new Array(buckets);
   }
 
   insert(key, value) {
     let index = this.getHash(key);
     const temp = new HashNode(key, value);
+    let startIndex = index;
 
-    // when the index slot is empty
-    if (this.#table[index] == null) {
-      return this.#table[index] = temp;
-    }
-    // when slot is occupied continuously probe the index to find empty slot
-    while (this.#table[index] !== null) {
-        console.log("probing")
+    // when slot is occupied, not deleted and does not have the same key continuously probe
+    while (
+      this.#table[index] !== undefined &&
+      this.#table[index] !== DELETED &&
+      this.#table[index].key !== key
+    ) {
       index = (index + 1) % this.#bucketsCount;
+      // pevent infinite loop
+      if (index === startIndex) break;
     }
 
     // assign value at empty slot
@@ -33,58 +37,65 @@ class HashTable {
 
   get(key) {
     let index = this.getHash(key);
+    let startIndex = index;
 
-    // when empty
-    if (this.#table[index] === null) return -1;
-    // when key matches return its valu
-    if (this.#table[index].key === key) return this.#table[index].value;
-    // linear probe until u find natching ket
-    while (this.#table[index].key !== key ) {
+    // linear probe as long as slot is not empty
+    while (this.#table[index] !== undefined) {
+      // if key is found after probing
+      if (this.#table[index] !== DELETED && this.#table[index].key === key) {
+        return this.#table[index].value;
+      }
+      // probe index(when key is not found)
       index = (index + 1) % this.#bucketsCount;
+      // prevent infinite loop(when done the index circles back to front)
+      if (index === startIndex) break;
     }
-    // if key is found after probing
-    if (this.#table[index].key === key) return this.#table[index].value;
     // when key doesn't exists
     return -1;
   }
 
   delete(key) {
     let index = this.getHash(key);
+    let startIndex = index;
 
-    // when empty
-    if (this.#table[index] === null) return false;
-
-    if (this.#table[index].key === key) {
-      this.#table[index] = { key: -1, value: -1 };
-      return true;
-    }
     // linear probe
-    while (this.#table[index].key !== key) {
+    while (this.#table[index] !== undefined) {
+      // simulate deletion]
+      if (this.#table[index] !== DELETED && this.#table[index].key === key) {
+        this.#table[index] = DELETED;
+        return true;
+      }
       index = (index + 1) % this.#bucketsCount;
-    }
-    if (this.#table[index].key === key) {
-      this.#table[index] = { key: -1, value: -1 };
-      return true;
+      //prevent infinate loop
+      if (index === startIndex) break;
     }
     // not found
     return false;
   }
 
   getHash(key) {
-    return key % this.bucketsCount;
+    return key % this.#bucketsCount;
   }
 
   getTable() {
-    console.log(this.#table)
+    console.log(this.#table);
   }
 }
 
-const hashtable = new HashTable(4)
-const list = [[1, 2], [3, 4], [5, 6]]
-hashtable.insert(10, 20)
-hashtable.insert(10, 20)
-hashtable.insert(10, 20)
-hashtable.insert(10, 20)
-// list.forEach((a, b) => hashtable.insert(a, b))
+const hashTable = new HashTable(4);
+const list = [
+  [10, 20],
+  [30, 40],
+  [50, 60],
+  [70, 80],
+];
+// hashtable.insert(10, 20);
+// hashtable.insert(30, 40);
+// hashtable.insert(50, 60);
+// hashtable.insert(70, 80);
 
-hashtable.getTable()
+list.forEach(([a, b]) => hashTable.insert(a, b));
+hashTable.delete(30);
+console.log("Get key 30: " + hashTable.get(30));
+hashTable.insert(90, 100);
+hashTable.getTable();
