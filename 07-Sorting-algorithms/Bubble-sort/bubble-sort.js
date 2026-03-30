f1c0436537ba95328a9c80f35705e7d8d9e92562
Evan ngugi wambui <evanngugi547@gmail.com>
2026-03-30 12:25:25 +0300

    update bubble sort

--- 07-Sorting-algorithms/Bubble-sort/bubble-sort.js
+++ 07-Sorting-algorithms/Bubble-sort/bubble-sort.js
@@ -1,22 +1,41 @@
 function bubbleSort(arr, n) {
     // pass left to right (array size bounded)
-    for(let pass = 0; pass < n - 1; pass++) {
+for(let i = 0; i < n - 1; i++) {
+    let flag = 0; // track swaps  
         // with each pass access and compare adjacent elements(using comparison operator)
-        for(let i = 0; i < n ; i++) {
-            const j = i + 1 // adjacency(problem we are accessing outside memory)
+        for(let j = 0;  j < n - i - 1; j++) {
             // current element is greater than the next
-            if(arr[i] > arr[j]) {
+            if(arr[j] > arr[j + 1]) {
                 // swap
-                const temp = arr[i]
-                arr[i] = arr[j]
-                arr[j] = temp
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+
+                flag++
             }
         }
-
+    
+        // if their no swaps after first pass then exit(list is already sorted)
+        if(flag == 0) break;
     }
     return arr
 }
 
+function generateInputs(length) {
+    const arr = []
+    for(let i = 0; i < length; i++) {
+        arr[i] = Math.round(Math.random() * length);
+    }
+    return arr;
+}
+
+console.log(process.pid)
+
 let arr = [100, 20, 340, 4, 700, 1];
+// arr = generateInputs(50_000);
+// console.log(arr);
+
+// console.time("Bubble sort")
 arr = bubbleSort(arr, arr.length)
+// console.timeEnd("Bubble sort")
 console.log(arr)
