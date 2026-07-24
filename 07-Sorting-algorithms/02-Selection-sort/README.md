e76948ef50c9060206b31b4683fedac14872eff9
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-24 20:55:54 +0300

    add merge sort

---
+++ 07-Sorting-algorithms/02-Selection-sort/README.md
@@ -0,0 +1,12 @@
+# Selection sort
+- Splits the list of items into two parts, sorted and remaining unsorted part
+- When it starts their is no data in the sorted part since no data has been sorted
+- Finds the smallest element of the unsorted part and swaps it with the left most element in the sorted array
+- Repeats itself by finding the smallest element in the unsorted list and swaps it with the leftmost element
+
+### Pseudocode
+```text
+procedure selectionSort(A: list of unsorted items)  
+    repeat
+        
+```
---
+++ 07-Sorting-algorithms/04-Merge-sort/mergesort.js
@@ -0,0 +1,50 @@
+function mergeSort(list) {
+  const size = list.length;
+
+  // base case(list has only one element is considered sorted)
+  if (size === 1) {
+    return;
+  }
+
+  /* __________ Divide ___________ */
+  // split list into halves when elements are greater than one
+  const midPoint = Math.floor(size / 2);
+  const leftList = list.slice(0, midPoint);
+  const rightList = list.slice(midPoint);
+
+  // recursively both the left and right list into halves
+  mergeSort(leftList);
+  mergeSort(rightList);
+
+  /* ______ Conquer ______ */
+  // merge while sorting the pieces together]
+  merge(leftList, rightList, list);
+  // return the fully sorted array
+  return list;
+}
+
+function merge(leftList, rightList, list) {
+  let index = 0;
+
+  while (leftList.length && rightList.length) {
+    if (rightList[0] < leftList[0]) {
+      list[index++] = rightList.shift();
+    } else {
+      list[index++] = leftList.shift();
+    }
+  }
+
+  while(leftList.length) {
+    console.log("Left list is: ", leftList)
+    list[index++] = leftList.shift()
+  }
+  while(rightList.length) {
+    console.log("Right list is: ", rightList)
+    list[index++] = rightList.shift()
+  }
+
+  console.log("**End of merge** list is ", list)
+}
+
+const arr = [5, 1, 7, 3, 2, 8, 6, 4, 12]
+mergeSort(arr)
