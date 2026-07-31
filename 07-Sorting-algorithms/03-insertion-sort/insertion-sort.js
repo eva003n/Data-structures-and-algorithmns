22645f31f064ce9ef3c7ba4619f0c6e86f7dd607
Evan ngugi wambui <evanngugi547@gmail.com>
2026-07-31 12:15:10 +0300

    fix bug in insertion sort

--- 07-Sorting-algorithms/03-insertion-sort/insertion-sort.js
+++ 07-Sorting-algorithms/03-insertion-sort/insertion-sort.js
@@ -7,11 +7,12 @@ function insertionSort(arr) {
       let x = arr[i];// current element
       while (j > 0 && arr[j - 1] > x) {
         swap(j, arr);
+        // makes sure the element is in the correct position
         j = j - 1;
       }
-      // rightmost element is larger maintain its position
+      // smallest left shits to the left
       arr[j] = x; 
-
+      // move to the next
       i = i + 1;
     }
 
@@ -19,23 +20,10 @@ function insertionSort(arr) {
 
 }
 
-function insertionSortR(arr, n) {
-    if(n > 0) {
-        insertionSort(arr, n - 1)
-        const x = arr[n]
-        let j = n - 1
-        while(j >= 0 && arr[j] > x) {
-            arr[j + 1]= arr[j]
-            j = j - 1
-        }
-        arr[j + 1] = x
 
-    }
-
-}
 
 function swap(j, arr) {
-// shit the element to the right
+// shift the largest element to the right
   arr[j] = arr[j - 1]; 
 }
 
@@ -43,4 +31,4 @@ let arr = [
   842, 153, 967, 421, 75, 689, 234, 918, 506, 312, 47, 781, 199, 654, 890, 28,
   473, 135, 799, 560,
 ];
-console.log(insertionSort(arr))
+console.log(insertionSortR(arr))
